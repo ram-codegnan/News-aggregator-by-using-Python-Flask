e19503ab96c142ec9521c7ef2e33069ec6e1b111
ram-codegnan <113403992+ram-codegnan@users.noreply.github.com>
2023-06-17 13:06:21 +0530

    Update cmail.py

--- cmail.py
+++ cmail.py
@@ -2,9 +2,9 @@ import smtplib
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('rambandaru555@gmail.com','kqfvocdzmymveugn')
+    server.login('youmail@gmail.com','yourkey')
     msg=EmailMessage()
-    msg['From']='rambandaru555@gmail.com'
+    msg['From']='your mail@gmail.com'
     msg['Subject']=subject
     msg['To']=to
     msg.set_content(body)
